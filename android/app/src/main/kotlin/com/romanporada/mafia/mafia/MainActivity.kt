f5a796adbd42ea52fccf37808264e7e873e349b9
RomanPorada <romanporada32@gmail.com>
2024-02-15 13:38:46 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/com/romanporada/mafia/mafia/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.romanporada.mafia.mafia
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/game_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:mafia/main.dart';
+
+class GameScreen extends StatelessWidget {
+  const GameScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+        backgroundColor: backgroundColor,
+        body: Center(
+      child: Text("Гра почалась"),
+    ));
+  }
+}
